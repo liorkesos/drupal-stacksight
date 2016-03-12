4ca4cd51e7b574bb885a2b8dff4dbb57952fd289
Lior Kesos <lior@linnovate.net>
2016-03-12 22:06:06 +0200

    bump

--- settings.php
+++ settings.php
@@ -8,9 +8,9 @@ if (file_exists($local_settings)) {
 
 $vars= json_decode(base64_decode(getenv("PLATFORM_VARIABLES")));
 if (isset($vars->STACKSIGHT_TOKEN)){
+  dpm($vars->STACKSIGHT_TOKEN);
   $stacksight_bootstrap =  DRUPAL_ROOT . '/sites/all/modules/contrib/stacksight' . '/stacksight-php-sdk/bootstrap-drupal.php';
   if (file_exists($stacksight_bootstrap)) {
-  define('STACKSIGHT_APP_ID',$vars->STACKSIGHT_APP_ID);
 // Just a comment
   define('STACKSIGHT_TOKEN', $vars->STACKSIGHT_TOKEN);
   require_once(DRUPAL_ROOT . '/sites/all/modules/contrib/stacksight' . '/stacksight-php-sdk/bootstrap-drupal.php');
